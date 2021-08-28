8f55293546794fd9f34404c0acf97a54dc4c04d3
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-08-29 01:15:34 +0200

    add: post controller logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,29 @@
-const getPosts = (req, res) => {
-    res.send('THIS WORKS')
+const {PostMessage} = require('../models/postMessage.js') 
+
+const getPosts = async (req, res) => {
+    try{
+        const postMessage = await PostMessage.find()
+        res.status(200).json(postMessage)
+
+    } catch(err){
+        res.status(404).json({message: err.message})
+    }
 }
 
+const createPost = async (req, res) => {
+    const post = req.body
+    const newPost = new PostMessage(post)
+
+    try{
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch(err){
+        res.status(409).json({message: err.message})
+    }
+}
+
+
 module.exports = {
-    getPosts
+    getPosts,
+    createPost,
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {getPosts} = require('../controllers/posts.js')
+const {getPosts, createPost} = require('../controllers/posts.js')
 
 const router = express.Router()
 
 router.get('/', getPosts)
+router.get('/', createPost)
 
 module.exports = {
     postRoutes: router
