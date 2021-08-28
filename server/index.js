c41369d6e26cfe53bebca5330d43b868c405a162
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-08-29 00:54:30 +0200

    add: server set up

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const moongose = require('moongose')
+const mongoose = require('mongoose')
 const cors = require('cors');
+
+const app = express();
+
+app.use(express.json({limit: '30mb', extended: true}));
+app.use(express.urlencoded({limit: '30mb', extended: true}));
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://bader:QH9cMDELW91OdECC@cluster0.iasbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
+    .catch(err => console.error(err.message))
