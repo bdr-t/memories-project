85bf97d0fd058f40060b5b684a901734d36e56f8
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-08-30 00:00:08 +0200

    add packages

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const cors = require('cors');
+const dotenv = require('dotenv').config()
 
 const {postRoutes} = require('./routes/posts.js')
 
@@ -13,9 +14,9 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://bader:QH9cMDELW91OdECC@cluster0.iasbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://bader:QH9cMDELW91OdECC@cluster0.iasbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch(err => console.error(err.message))
