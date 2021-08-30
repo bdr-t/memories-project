eb1a29d76d7b938cf5362d413b1cc663eaa91a7b
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-08-30 13:45:06 +0200

    add: fix like and delete

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,81 +1,146 @@
-import {useState, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {createPost, updatePost} from '../../actions/posts'
-import useStyles from './styles'
-import FileBase from 'react-file-base64'
-import { TextField, Button, Typography, Paper} from '@material-ui/core';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createPost, updatePost } from "../../actions/posts";
+import useStyles from "./styles";
+import FileBase from "react-file-base64";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
-const Form = ({currentId, setCurrentId}) => {
-    //styles
-    const classes = useStyles()
+const Form = ({ currentId, setCurrentId }) => {
+  //styles
+  const classes = useStyles();
 
-    //dispatch
-    const dispatch = useDispatch()
+  //dispatch
+  const dispatch = useDispatch();
 
-    //state
-    const [postData, setPostData] = useState({
-        creator: '',
-        title: '',
-        message: '',
-        tags: '',
-        selectedFile:'',
-    })
+  //state
+  const [postData, setPostData] = useState({
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const user = JSON.parse(localStorage.getItem("profile"));
 
-    //selector
-    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  //selector
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
 
+  //Effects
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
-    //Effects
-    useEffect(() => {
-        if(post) setPostData(post)
-    }, [post])
+  //actions
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    //actions
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        if(currentId){
-            dispatch(updatePost(currentId, postData, setPostData))
-        } else{
-            dispatch(createPost(postData))
-        }
-        clear()
+    if (currentId === 0) {
+      dispatch(
+        updatePost(currentId, { ...postData, name: user?.result?.name })
+      );
+      clear();
+    } else {
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
+      clear();
     }
+  };
 
-    const clear = () => {
-        setCurrentId(null)
-        setPostData({
-            creator: '',
-            title: '',
-            message: '',
-            tags: '',
-            selectedFile:'',
-        })
-    }
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories and like other's memories.
+        </Typography>
+      </Paper>
+    );
+  }
 
-    return ( 
-        <Paper className={classes.paper}>
-            <form autoComplete='off' noValidate className={`${classes.form} ${classes.root}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>{currentId ? 'Editing': 'Creating'} a Memory</Typography>
-                <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+  const clear = () => {
+    setCurrentId(null);
+    setPostData({
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
 
-                <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e)=> setPostData({...postData, title: e.target.value})}/>
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.form} ${classes.root}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          {currentId ? "Editing" : "Creating"} a Memory
+        </Typography>
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
 
-                <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e)=> setPostData({...postData, message: e.target.value})}/>
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
 
-                <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e)=> setPostData({...postData, tags: e.target.value.split(',')})}/>
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
+        />
 
-                <div className={classes.fileInput}>
-                    <FileBase type='file' multiple={false} onDone={({base64})=> setPostData({...postData, selectedFile: base64})}/>
-                </div>
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
 
-                <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
 
-                <Button className={classes.buttonSubmit} variant='contained' color='secondary' size='small' onClick={clear}>Clear</Button>
-            </form>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
+};
 
-        </Paper>
-     );
-}
- 
 export default Form;
