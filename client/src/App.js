ca521922c8ef493ea478b4cc5974b54de155a0b0
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-08-30 14:36:20 +0200

    add: pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Container } from "@material-ui/core";
 import NavBar from "./components/NavBar/NavBar";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 
@@ -10,8 +10,11 @@ function App() {
       <Container maxWidth="lg">
         <NavBar />
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
           <Route path="/auth" exact component={Auth} />
+          {/* <Route path="/posts/:id" exact component={PostDetails} /> */}
         </Switch>
       </Container>
     </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
-import {Container, Grow, Grid} from '@material-ui/core'
+import {Container, Grow, Grid, Paper} from '@material-ui/core'
 import Posts from '../Posts/Posts'
 import Form from "../Form/Form"
 import {useDispatch} from 'react-redux'
 import {useEffect, useState} from 'react'
 import {getPosts} from '../../actions/posts'
 import useStyles from '../../App.styles'
+import Paginate from '../Pagination'
+
 
 
 
@@ -32,7 +34,9 @@ const Home = () => {
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId}/>
-
+              <Paper className={classes.pagination} elevation={6}>
+                <Paginate/>
+              </Paper>
             </Grid>
           </Grid>
         </Container>
